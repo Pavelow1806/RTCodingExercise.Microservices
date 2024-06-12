348a2cd6e9d42e0b425de2efb10da39763dccf65
James Coyle <33148421+Pavelow1806@users.noreply.github.com>
2024-06-12 20:52:18 +0100

    Created services folder and response domain objects as well as added a table on the frontend to display plates

--- src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
@@ -1,7 +1,15 @@
-﻿namespace Catalog.API.Controllers
+﻿using Catalog.Domain.Responses;
+
+namespace Catalog.API.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        [Route("plates")]
+        public async Task<PlatesResponse> GetPlates(CancellationToken cancellationToken)
+        {
+
+        }
         public IActionResult Index()
         {
             return new RedirectResult("~/swagger");
---
+++ src/Services/Catalog/Catalog.Domain/Responses/PlatesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Catalog.Domain.Responses
+{
+    public class PlatesResponse
+    {
+        public List<Plate> Plates { get; set; } = new List<Plate>();
+    }
+}
